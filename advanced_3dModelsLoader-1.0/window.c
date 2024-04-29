06ec9d04e0f7c43bbbac3a84a46344c506f8d81a
Hermione <Hermione@nacer.dz>
2024-04-29 22:11:16 +0200

    ✏️ load police car model

--- advanced_3dModelsLoader-1.0/window.c
+++ advanced_3dModelsLoader-1.0/window.c
@@ -44,7 +44,7 @@ void init(void)
   /* charger 3 modèles différents */
   _id_modele[0] = assimpGenScene("models/soccer/soccerball.obj");
   // _id_modele[1] = assimpGenScene("models/nixanz.3ds");
-  _id_modele[1] = assimpGenScene("models/nixanz.3ds");
+  _id_modele[1] = assimpGenScene("models/police_car/scene.gltf");
   // _id_modele[2] = assimpGenScene("models/balloon/balloon_low.obj");
   _id_modele[2] = assimpGenScene("models/Snapshooter/scene.gltf");
 
