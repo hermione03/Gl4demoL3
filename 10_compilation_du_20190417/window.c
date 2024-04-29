20b7ed1dcd87c8dc6b22e06a4bee78596774cb2b
Hermione <Hermione@nacer.dz>
2024-04-29 05:11:42 +0200

    ✨ julia fractale

--- 10_compilation_du_20190417/window.c
+++ 10_compilation_du_20190417/window.c
@@ -28,26 +28,30 @@ static void keydown(int keycode);
  * ce tableau se termine toujours par l'élémént {0, NULL, NULL,
  * NULL} */
 static GL4DHanime _animations[] = {
-  { 5000, transformations, NULL, NULL },
-  { 5000, transformations, mix0105, fondu },
-  { 5000, mix0105, NULL, NULL },
-  {    0, NULL, NULL, NULL } /* Toujours laisser à la fin */
+    {4000, julia, NULL, NULL},
+    {10000, julia, NULL, transformations},
+    {5000, transformations, NULL, NULL},
+    {5000, transformations, mix0105, fondu},
+    {5000, mix0105, NULL, NULL},
+    {0, NULL, NULL, NULL} /* Toujours laisser à la fin */
 };
 
 /*!\brief dimensions initiales de la fenêtre */
-//static GLfloat _dim[] = {1024, 768};
+// static GLfloat _dim[] = {1024, 768};
 static GLfloat _dim[] = {1280, 900};
 
 /*!\brief fonction principale : initialise la fenêtre, OpenGL, audio
  * et lance la boucle principale (infinie).
  */
-int main(int argc, char ** argv) {
-  if(!gl4duwCreateWindow(argc, argv, "GL4Dummies DemoHelper", 
-			 0, 0, 
-			 _dim[0], _dim[1],
-			 GL4DW_RESIZABLE | /* GL4DW_FULLSCREEN | */GL4DW_SHOWN))
+int main(int argc, char **argv)
+{
+  if (!gl4duwCreateWindow(argc, argv, "GL4Dummies DemoHelper",
+                          0, 0,
+                          _dim[0], _dim[1],
+                          GL4DW_RESIZABLE | /* GL4DW_FULLSCREEN | */ GL4DW_SHOWN))
     return 1;
-  if(TTF_Init() == -1) {
+  if (TTF_Init() == -1)
+  {
     fprintf(stderr, "TTF_Init: %s\n", TTF_GetError());
     exit(2);
   }
@@ -66,7 +70,8 @@ int main(int argc, char ** argv) {
  * ainsi que les divers données et fonctionnalités liées à la gestion
  * des animations.
  */
-static void init(void) {
+static void init(void)
+{
   glClearColor(0.2f, 0.2f, 0.2f, 0.0f);
   gl4dhInit(_animations, _dim[0], _dim[1], animationsInit);
   resize(_dim[0], _dim[1]);
@@ -77,26 +82,32 @@ static void init(void) {
  * \param w largeur de la fenêtre.
  * \param w hauteur de la fenêtre.
  */
-static void resize(int w, int h) {
-  _dim[0] = w; _dim[1] = h;
+static void resize(int w, int h)
+{
+  _dim[0] = w;
+  _dim[1] = h;
   glViewport(0, 0, _dim[0], _dim[1]);
 }
 
 /*!\brief permet de gérer les évènements clavier-down.
  * \param keycode code de la touche pressée.
  */
-static void keydown(int keycode) {
-  switch(keycode) {
+static void keydown(int keycode)
+{
+  switch (keycode)
+  {
   case SDLK_ESCAPE:
   case 'q':
     exit(0);
-  default: break;
+  default:
+    break;
   }
 }
 
 /*!\brief appelée à la sortie du programme (atexit).
  */
-static void quit(void) {
+static void quit(void)
+{
   ahClean();
   gl4duClean(GL4DU_ALL);
 }
